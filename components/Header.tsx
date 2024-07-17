1ee2a473e3547a31b03baf13dc4df3f1dff31001
Daani Danish <legit.daani@gmail.com>
2024-07-17 23:12:32 +0500

    Header: Logo, search, themeToggler, GenreDropDown

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,27 @@
 import Image from "next/image"
 import Link from "next/link"
+import { ThemeMode } from "./ThemeMode"
+import SearchInput from "./SearchInput"
+import GenreDropDown from "./GenreDropDown"
 
 function Header() {
   return (
-    <header>
-        <Link href = "/">
+    <header className="fixed w-full z-20 top-0 flex items-center justify-between p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900">
+        <Link href = "/" className="mr-10">
            <Image 
            src="https://links.papareact.com/a943ae"
            alt="Disney logo"
             height={120}
             width={100}
-            className="cursor-pointer invert"
+            className="cursor-pointer invert-0 dark:invert"
            />
         </Link>
 
         <div className="flex space-x-2">
             {/* genre Dropdown */}
-            {/* Search Input */}
-            {/* dark mode toggle */}
+            <GenreDropDown />
+            <SearchInput />
+            <ThemeMode />
         </div>
     </header>
   )
